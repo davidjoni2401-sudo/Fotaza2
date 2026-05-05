147c6a3f6df62469903c5a78918c840631506e7f
jonatan <david.joni2401@gmail.com>
2026-05-04 22:32:00 -0300

    Hasta ahorra son 4 pasos:
    1) Creo el proyecto,
    2) Instalo las dependencias (node, express, pug y nodemon).
    3) Estructura,
    4) Código de app.js

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import session from "express-session";
+
+import authRoutes from "./routes/authRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+
+app.set("view engine", "pug");
+app.set("views", path.join(process.cwd(), "src/views"));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(session({
+    secret: "fotaza_secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(express.static("public"));
+
+
+app.use("/", authRoutes);
+app.use("/posts", postRoutes);
+
+
+app.get("/", (req, res) => {
+    res.send("Fotaza 2 funcionando 🚀");
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor en http://localhost:${process.env.PORT}`);
+});
