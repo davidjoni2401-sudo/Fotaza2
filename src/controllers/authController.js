2f69ec3ed2d3ed53c9a6b01e34733e2ec9f94e54
jonatan <david.joni2401@gmail.com>
2026-06-01 14:51:52 -0300

    Modificaciones en:
    1) Login verifica cuenta inactiva y guarda rol en sesión: src\controllers\authController.js,

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,12 +6,12 @@ export const showRegister = (req, res) => {
 };
 
 
-export const register = async(req, res) => {
-    try{
+export const register = async (req, res) => {
+    try {
         const { nombre, email, password } = req.body;
 
 
-        const hashedPassword = await bcrypt.hash(password, 10);  
+        const hashedPassword = await bcrypt.hash(password, 10);
 
 
         await createUser(nombre, email, hashedPassword);
@@ -19,10 +19,10 @@ export const register = async(req, res) => {
 
         res.send("Usuario registrado ✅");
 
-    }catch(error) {
+    } catch (error) {
         console.log(error);
 
-        if(error.code === "23505"){
+        if (error.code === "23505") {
             return res.send("Ese email ya esta registrado ❌");
         }
         res.send("Error al registarte como usuario ❌");
@@ -36,31 +36,35 @@ export const showLogin = (req, res) => {
 export const login = async (req, res) => {
 
     try {
-
         const { email, password } = req.body;
-
         const result = await findUserByEmail(email);
-
-        if(result.rows.length === 0) {
+        if (result.rows.length === 0) {
             return res.send("Usuario no existe ❌");
         }
-
         const user = result.rows[0];
+
+        if (user.estado === "inactivo") {
+            return res.send("Tu cuenta fue desactivada por incumplimiento de las normas ❌");
+        }
 
         const validPassword = await bcrypt.compare(
             password,
             user.password
         );
-
-        if(!validPassword) {
-            return res.send("Contraseña incorrecta ❌")
+        if (!validPassword) {
+            return res.send("Contraseña incorrecta ❌");
         }
 
-        req.session.user = user;
+        req.session.user = {
+            id: user.id,
+            nombre: user.nombre,
+            email: user.email,
+            rol: user.rol,
+            estado: user.estado
+        };
+        res.redirect("/posts/feed");
 
-        res.send(`Bienvenido ${user.nombre} ✅`);
-
-    }catch(error) {
+    } catch (error) {
 
         console.log(error);
 
