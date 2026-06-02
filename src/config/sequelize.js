b24717a799a5da6278707fe4e3d520b4a7575c5c
jonatan <david.joni2401@gmail.com>
2026-06-01 21:04:20 -0300

    Requiere SSL2: para conexión con Render y corregir isProduction
    src\config\sequelize.js

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const isProduction = process.env.DB_HOST && process.env.DB_HOST.includes("render.com");
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
